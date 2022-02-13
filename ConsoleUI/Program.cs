33d97f12137c586db1a428f75aba7067a2aa8f17
Furkan Demir <fd2366131@gmail.com>
2022-02-13 18:57:48 +0300

    Add project files.

---
+++ ConsoleUI/Program.cs
@@ -0,0 +1,30 @@
+﻿using Business.Concrete;
+using DataAccess.Concrete.EntityFramework;
+using DataAccess.Concrete.InMemory;
+using System;
+
+namespace ConsoleUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ProductManager productManager = new ProductManager(new EfProductDal());
+
+            foreach (var product1 in productManager.GetAll())
+            {
+                //Console.WriteLine(product.ProductName);
+            }
+
+            foreach (var product2 in productManager.GetAllByCategoryId(6))
+            {
+                //Console.WriteLine(product2.ProductName);
+            }
+
+            foreach (var product3 in productManager.GetByUnitPrice(10,50))
+            {
+                Console.WriteLine(product3.ProductName + " : " + product3.UnitPrice);
+            }
+        }
+    }
+}
