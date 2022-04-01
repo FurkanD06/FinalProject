e894fe34e8383b682009edf2978096907791e616
Furkan Demir <fd2366131@gmail.com>
2022-04-01 22:21:02 +0300

    FinalProject

--- Business/Constants/Messages.cs
+++ Business/Constants/Messages.cs
@@ -1,6 +1,8 @@
-﻿using Entities.Concrete;
+﻿using Core.Entities.Concrete;
+using Entities.Concrete;
 using System;
 using System.Collections.Generic;
+using System.Runtime.Serialization;
 using System.Text;
 
 namespace Business.Constants
@@ -14,5 +16,12 @@ namespace Business.Constants
         public static string ProductConutOfCategoryError = "Bir kategoride en fazla 10 ürün olabilir";
         public static string ProductNameAlreadyExists = "Aynı isimden ürün olamaz";
         public static string CategoryLimitExceded = "Kategori limiti aşıldığı için yemi ürün eklenemiyor";
+        public static string AuthorizationDenied = "Yetkiniz yok.";
+        public static string UserRegistered = "Kayıt olundu.";
+        public static string UserNotFound = "Kullanıcı bulunamadı";
+        public static string PasswordError = "Parola hatası";
+        public static string SuccessfulLogin = "Başarılı giriş";
+        public static string UserAlreadyExists = "Kullanıcı mevcut";
+        public static string AccessTokenCreated = "Token oluşturuldu";
     }
 }
--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -40,6 +40,9 @@ namespace ConsoleUI
             }
 
             
+
+
+
         }
     }
 }
